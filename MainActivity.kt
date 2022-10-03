c3965204f1d3d63f471c6416ed442c926f4464a8
gmh4589 <41452060+gmh4589@users.noreply.github.com>
2022-10-03 14:55:56 +0300

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,16 +1,20 @@
-package com.example.c_application
+package com.example.charge_time_new
 
 import android.annotation.SuppressLint
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.annotation.RequiresApi
-import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 class MainActivity : AppCompatActivity() {
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n", "SimpleDateFormat")
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -18,37 +22,37 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
-        val monList = resources.getStringArray(R.array.months)
+        val monList = resources.getStringArray(R.array.month)
         val dayList = resources.getStringArray(R.array.days)
 
-        val button = button
+        val button: Button = findViewById(R.id.button)
+        val cur_mah: EditText = findViewById(R.id.enter_currency)
+        val bat_mah: EditText = findViewById(R.id.enter_vollume)
+        val result_field: TextView = findViewById(R.id.info_panel)
+
         button.setOnClickListener {
-            val tok = cur_mah.text.toString().toFloat()
+            val cur = cur_mah.text.toString().toFloat()
             val vol = bat_mah.text.toString().toFloat()
-            val result = vol * 1.2 / tok
-            val time_now = LocalDateTime.now()
+            val result = (vol * 1.2 / cur)
+            val timeNow = LocalDateTime.now()
+            val dateArrayNow = timeNow.toString().split("T", ":", ".", "-")
 
-            val elsps = time_now.plusHours(result.toLong())
-            val date = elsps.toString().substringBefore('T')
-            val now = time_now.toString().substringBefore('T')
-            val time = elsps.toString().substringAfter('T')
-            val time2 = time.substringBeforeLast(':')
-            val dt = date.substringAfter('-')
-            val time3 = date.substringAfterLast('-').toInt() - now.substringAfterLast('-').toInt()
-            val mon = dt.substringBefore('-').toInt() - 1
+            val finish = timeNow.plusHours(result.toLong())
+            val dateArray = finish.toString().split("T", ":", ".", "-")
+            //[0:год, 1:месяц, 2:день, 3:час, 4:мин, 5:сек, 6:доли]
 
-            val elp = if (time3 < 3) {
-                dayList[time3]
-                } else { "${dt.substringAfter('-')} ${monList[mon]}"
+            val h = dateArray[3]
+            val m = dateArray[4]
+            val rDay = ((result + dateArrayNow[3].toInt()) / 24).toInt()
+
+            val day = if (rDay < 3) {
+                dayList[rDay]
+            } else {
+                dateArray[2] + ' ' + monList[dateArray[1].toInt()]
             }
 
-            up_field.text = resources.getString(R.string.end_txt)
-            result_field.text = "${elp} в ${time2}"
+            result_field.text = "$day в $h:$m"
         }
     }
 }
-
-
-
-
 
